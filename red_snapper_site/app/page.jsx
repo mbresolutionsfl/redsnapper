55c440cafd9fdf125f263be3a966444567296d31
mbresolutionsfl <mbresolutionsfl@gmail.com>
2025-12-10 17:55:49 -0500

    Add files via upload

---
+++ red_snapper_site/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ minHeight: "100vh", background: "black", color: "white" }}>
+      <h1 style={{ fontSize: "3rem", color: "red", fontWeight: "bold", fontFamily: "Agency FB, Impact, sans-serif", padding: "20px" }}>
+        Red Snapper Realty
+      </h1>
+      <p style={{ paddingLeft: "20px" }}>Your deployment is ready.</p>
+    </div>
+  );
+}
